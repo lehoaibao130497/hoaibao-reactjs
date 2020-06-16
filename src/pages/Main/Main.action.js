c2487bd5c3a652f562cca3b27ee6369401c73624
Hoai Bao <lehoaibao130497@gmail.com>
2020-06-16 18:17:57 +0700

    adding redux

---
+++ src/pages/Main/Main.action.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import store from '../../store'
+export const PRODUCT_LIST_REQUEST = "PRODUCT_LIST_REQUEST"
+export const PRODUCT_LIST_SUCCESS = "PRODUCT_LIST_SUCCESS"
+export const PRODUCT_LIST_FAILURE = "PRODUCT_LIST_FAILURE"
+
+export function productListRequestAction() {
+  return {
+    type: PRODUCT_LIST_REQUEST
+  }
+}
+
+export function productListSuccessAction(data) {
+  return {
+    type: PRODUCT_LIST_SUCCESS,
+    data
+  }
+}
+
+export function productListFailureAction(error) {
+  return {
+    type: PRODUCT_LIST_FAILURE,
+    error: error
+  }
+}
+
+
+export function getProductsAction() {
+  return async (dispatch) => {
+    dispatch(productListRequestAction())
+
+    try {
+      const result = await axios({
+        method: 'GET',
+        url: 'https://min-shop.herokuapp.com/rest/product'
+      })
+    
+      dispatch(productListSuccessAction(result.data.data))
+
+    } catch(error) {
+      dispatch({
+        type: PRODUCT_LIST_FAILURE,
+        error: error
+      })
+    }
+  }
+}
