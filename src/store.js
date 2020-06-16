0b566f09724774545a08ccc7692c11fcaa42bfe6
Hoai Bao <lehoaibao130497@gmail.com>
2020-06-16 18:22:10 +0700

    adding axios to call api

--- src/store.js
+++ src/store.js
@@ -1,24 +1,53 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import productsReducer from './pages/Main/Main.reducer'
-import productDetailReducer from './pages/ProductDetail/ProductDetail.reducer'
-import loginReducer from './pages/Login/Login.reducer'
-import registerReducer from './pages/Register/Register.reducer'
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux'
 
-const store = createStore(
-  combineReducers({
-    productsReducer, 
-    productDetailReducer,
-    loginReducer,
-    registerReducer
-  }),
-  {},
-  composeWithDevTools(
-    applyMiddleware(
-      thunk
-    )
-  )
-)
+const initState = {
+  hoVaTen: "ABC",
+  soTienDangCo: 200000,
+  product: {}
+}
+
+function Reducer1(state = initState, action) {
+  // if(action.type === "GUI_TIEN") {
+  //   return {
+  //     ...state,
+  //     soTienDangCo: state.soTienDangCo + action.data
+  //   }
+  // }
+
+  // if(action.type === 'RUT_TIEN') {
+  //   return {
+  //     ...state,
+  //     soTienDangCo: state.soTienDangCo - action.data
+  //   }
+  // } 
+
+  // return {...state}
+  switch(action.type) {
+    case 'GUI_TIEN':
+      return {
+        ...state,
+        soTienDangCo: state.soTienDangCo + action.data
+      }
+    case 'RUT_TIEN':
+      return {
+        ...state,
+        soTienDangCo: state.soTienDangCo - action.data
+      }
+    case 'DONG_TAI_KHOAN':
+      return {
+        ...state,
+        soTienDangCo: 0
+      }
+      case 'ADD_PRODUCT':
+        return {
+          ...state,
+          product: action.data
+        }
+    default:
+      return {...state}
+  }
+}
+
+const store = createStore(Reducer1, {})
 
 export default store
